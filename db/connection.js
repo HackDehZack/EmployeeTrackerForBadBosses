7e4df5c5893dce73a88b6eaef8b9e3340b6ec81b
HackDehZack <Zackseriousemail@gmail.com>
2023-12-18 22:26:21 -0500

    Update MySQL password handling to hide my password hopefully lol

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,14 @@
-const mysql = require('mysql');
+require('dotenv').config();     //For .env file
 
+const mysql = require('mysql');
 
 
 // Create the database connection
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'root',          // ZACK: Be sure to add the MySQL username here!
-  password: 'password',     // ZACK: Be sure to add the MySQL password here!
+  user: 'root',
+  password: process.env.DB_PASSWORD,
   database: 'employeetrackerforbadbosses'
 });
 
