a27d450ae83899fb9043baa903c117b5614d54eb
HackDehZack <Zackseriousemail@gmail.com>
2023-12-19 14:17:53 -0500

    Add API routes for employee management on index/sever .js and localhost3000 port on express server

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const Employee = require('./employee');
+//^ this is the class from employee.js, if err occurs mind this note
+
+// Create an instance of the Express application
+const app = express();
+
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Create an instance of the Employee class
+const employee = new Employee();
+
+// Define API routes
+app.get('/employees', async (req, res) => {
+  try {
+    const employees = await employee.getAllEmployees();
+    res.json(employees);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while retrieving employees' });
+  }
+});
+
+app.post('/employees', async (req, res) => {
+  const { firstName, lastName, roleId, managerId } = req.body;
+  try {
+    const result = await employee.addEmployee(firstName, lastName, roleId, managerId);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while adding an employee' });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+// The last line of code above starts the Express server on port 3000.
+// This port number (3000) is specific to the Express server and is unrelated to the MySQL port (3306) used for the database connection.
