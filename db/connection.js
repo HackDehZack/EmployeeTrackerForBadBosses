30d44d35dd1f3c76bc1dc00c11d573f9528a27a6
HackDehZack <Zackseriousemail@gmail.com>
2023-12-22 12:13:10 -0500

    Update database password in connection.js (Typo)

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: process.env.DB_PASSWORD,
+  password: 'Password',
   database: 'employeetrackerforbadbosses'
 });
 
