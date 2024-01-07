255809ae1016e397afd62adb99b7b8961bca7885
HackDehZack <Zackseriousemail@gmail.com>
2024-01-06 22:35:52 -0500

    Refactor database queries and main menu display

--- db/queries.js
+++ db/queries.js
@@ -2,46 +2,52 @@ const connection = require('./connection');
 
 // Function to retrieve all departments from the database
 function getAllDepartments() {
-    return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM department', (err, departments) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(departments);
-        }
-      });
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM department', (error, departments) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(departments);
+      }
     });
-  }
-  
-  // Function to add a new department to the database
-  function addDepartment(name) {
-    return new Promise((resolve, reject) => {
-      connection.query('INSERT INTO department SET ?', { name }, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
+  });
+}
+
+// Function to add a new department to the database
+function addDepartment(name) {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO department SET ?';
+    const departmentData = { name };
+
+    connection.query(query, departmentData, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
     });
-  }
-  
-  // Function to update a department in the database
-  function updateDepartment(id, name) {
-    return new Promise((resolve, reject) => {
-      connection.query('UPDATE department SET name = ? WHERE id = ?', [name, id], (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
+  });
+}
+
+// Function to update a department in the database
+function updateDepartment(id, name) {
+  return new Promise((resolve, reject) => {
+    const query = 'UPDATE department SET name = ? WHERE id = ?';
+    const departmentData = [name, id];
+
+    connection.query(query, departmentData, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
     });
-  }
-  
-  // Export the functions for other modules to use
-  module.exports = {
-    getAllDepartments,
-    addDepartment,
-    updateDepartment,
-  };
+  });
+}
+
+// Export the functions for other modules to use
+module.exports = {
+  getAllDepartments,
+  addDepartment,
+  updateDepartment,
+};
--- views/mainMenu.js
+++ views/mainMenu.js
@@ -3,41 +3,43 @@ const inquirer = require('inquirer');
 const Employee = require('../models/employee');
 
 // Function to display the main menu options
-function displayMainMenu() {
-  inquirer
-    .prompt([
+async function displayMainMenu() {
+  let exit = false;
+
+  while (!exit) {
+    const answers = await inquirer.prompt([
       {
         type: 'list',
         name: 'menuOption',
         message: 'What would you like to do?',
         choices: ['View all employees', 'Add an employee', 'Update an employee role', 'Exit'],
       },
-    ])
-    .then((answers) => {
-      // Implement the logic for each menu option here
-      switch (answers.menuOption) {
-        case 'View all employees':
-          // Call the method to view all employees
-          viewAllEmployees();
-          break;
-        case 'Add an employee':
-          // Call the method to add an employee
-          addEmployee();
-          break;
-        case 'Update an employee role':
-          // Call the method to update an employee's role
-          updateEmployeeRole();
-          break;
-        case 'Exit':
-          // Exit the application
-          console.log('Goodbye!');
-          process.exit(0);
-          break;
-        default:
-          console.log('Invalid option selected.');
-          break;
-      }
-    });
+    ]);
+
+    // Implement the logic for each menu option here
+    switch (answers.menuOption) {
+      case 'View all employees':
+        // Call the method to view all employees
+        await viewAllEmployees();
+        break;
+      case 'Add an employee':
+        // Call the method to add an employee
+        await addEmployee();
+        break;
+      case 'Update an employee role':
+        // Call the method to update an employee's role
+        await updateEmployeeRole();
+        break;
+      case 'Exit':
+        // Exit the application
+        console.log('Goodbye!');
+        exit = true;
+        break;
+      default:
+        console.log('Invalid option selected.');
+        break;
+    }
+  }
 }
 
 // Function to view all employees
@@ -49,8 +51,6 @@ async function viewAllEmployees() {
     const employees = await employee.getAllEmployees();
     // Display the employees
     console.log(employees);
-    // Display the main menu again
-    displayMainMenu();
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -60,9 +60,42 @@ async function viewAllEmployees() {
 // Function to add an employee
 async function addEmployee() {
   try {
-    // Implement the logic to add an employee here
-    // Display the main menu again
-    displayMainMenu();
+    // Create a new instance of the Employee class
+    const employee = new Employee();
+
+    // Prompt the user for employee details
+    const employeeDetails = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: "Enter the employee's first name:",
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: "Enter the employee's last name:",
+      },
+      {
+        type: 'input',
+        name: 'roleId',
+        message: "Enter the employee's role ID:",
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: "Enter the employee's manager ID:",
+      },
+    ]);
+
+    // Call the addEmployee method to add the employee
+    await employee.addEmployee(
+      employeeDetails.firstName,
+      employeeDetails.lastName,
+      employeeDetails.roleId,
+      employeeDetails.managerId
+    );
+
+    console.log('Employee added successfully.');
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -72,9 +105,27 @@ async function addEmployee() {
 // Function to update an employee's role
 async function updateEmployeeRole() {
   try {
-    // Implement the logic to update an employee's role here
-    // Display the main menu again
-    displayMainMenu();
+    // Create a new instance of the Employee class
+    const employee = new Employee();
+
+    // Prompt the user for employee and role details
+    const employeeRoleDetails = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'employeeId',
+        message: "Enter the employee's ID:",
+      },
+      {
+        type: 'input',
+        name: 'roleId',
+        message: "Enter the new role ID:",
+      },
+    ]);
+
+    // Call the updateEmployeeRole method to update the employee's role
+    await employee.updateEmployeeRole(employeeRoleDetails.employeeId, employeeRoleDetails.roleId);
+
+    console.log('Employee role updated successfully.');
   } catch (error) {
     console.error(error);
     process.exit(1);
